510ac96c0631cefa5dffa9bf62540b2a66325d74
clanTV <108497073+clanTV@users.noreply.github.com>
2022-09-27 16:38:01 +0800

    Update PlayFragment.java

--- app/src/main/java/com/github/tvbox/osc/ui/fragment/PlayFragment.java
+++ app/src/main/java/com/github/tvbox/osc/ui/fragment/PlayFragment.java
@@ -456,10 +456,10 @@ public class PlayFragment extends BaseLazyFragment {
                 mVodPlayerCfg.put("sp", 1.0f);
             }
             if (!mVodPlayerCfg.has("st")) {
-                mVodPlayerCfg.put("st", 0);
+                mVodPlayerCfg.put("st", 120);
             }
             if (!mVodPlayerCfg.has("et")) {
-                mVodPlayerCfg.put("et", 0);
+                mVodPlayerCfg.put("et", 120);
             }
         } catch (Throwable th) {
 
